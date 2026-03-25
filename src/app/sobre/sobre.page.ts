ca7b94c0eeb5e53a6f48b177ee07617efb76536e
Valentim.dev <sdvr2017@gmail.com>
2026-03-25 01:45:32 -0300

    comit

---
+++ src/app/sobre/sobre.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-sobre',
+  templateUrl: './sobre.page.html',
+  styleUrls: ['./sobre.page.scss'],
+  standalone: true,
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonButton, RouterLink, CommonModule, FormsModule]
+})
+export class SobrePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
